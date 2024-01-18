23439a70aa6b550d783f5c0e4fe5eee81be7cddc
EgorPvv <145757987+EgorPvv@users.noreply.github.com>
2024-01-18 12:33:32 +0300

    Update script.js
    • Добавлен функционал кнопке закрытия области всплывающих уведомлений

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 'use strict';
 
+let cross_button = document.querySelector('.cross-button');
+
+function closeAlerts() {
+    let alerts = document.querySelector(".alerts");
+    alerts.style.display = "none";
+};
+
+cross_button.addEventListener("click", closeAlerts);
+
 let perPage = 4;
 let currentPage = 1;
 let totalPage = 0;
